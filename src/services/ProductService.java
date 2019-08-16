47c63da0e791d3bdac5cb1c701d2c60c31f3fd91
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-08-16 14:19:09 -0300

    1 - Function with args

---
+++ src/services/ProductService.java
@@ -0,0 +1,16 @@
+package services;
+import java.util.List;
+
+import entities.Product;
+
+public class ProductService {
+	public double filteredSum(List<Product> list) {
+		double sum = 0.0;
+		for(Product p: list) {
+			if(p.getName().charAt(0) == 'T') {
+				sum += p.getPrice();
+			}
+		}
+		return sum;
+	}
+}
